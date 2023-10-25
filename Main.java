283ecc0c8803ef8036c0b2d9fe9e096710e4e46b
Shelly <shellykurul@gmail.com>
2023-10-25 18:29:24 +0800

    simple java

--- Main.java
+++ Main.java
@@ -1,12 +1,16 @@
 import java.util.Scanner;
 
-public class Main {
+public class InformasiPengguna {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner input = new Scanner(System.in);
 
-        System.out.print("Masukkan nama Kamu: ");
-        String nama = scanner.nextLine();
+        System.out.print("Masukkan nama kamu: ");
+        String nama = input.nextLine();
 
-        System.out.println("Hai, " + nama + " Selamat Datang di GIT!");
+        System.out.print("Masukkan umur kamu: ");
+        int umur = input.nextInt();
+
+        System.out.println("Halo, " + nama + "!");
+        System.out.println("Kamu berusia " + umur + " tahun.");
     }
 }
